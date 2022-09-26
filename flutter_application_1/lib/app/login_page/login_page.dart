6ec6289cfbf89af8fbee3f888c35418a146f69b9
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-09-26 12:38:27 +0530

    login page

--- flutter_application_1/lib/app/login_page/login_page.dart
+++ flutter_application_1/lib/app/login_page/login_page.dart
@@ -10,6 +10,38 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page"),
+        leading: const Icon(
+          Icons.cabin,
+          color: Colors.white,
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        //in-side
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        // out-side
+        decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(4),
+                blurRadius: 2.0,
+                spreadRadius: 3.0
+              )
+            ]),
+        width: 200,
+        height: 100,
+        child: const Center(
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
   }
 }
