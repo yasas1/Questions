5bf714787079c4ff83d3f893d486f764dfb5c29e
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-10-24 22:26:05 +0530

    integration test demo application started

---
+++ demo/src/main/java/com/integration/test/demo/IntegrationTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.integration.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTestDemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/integration/test/demo/IntegrationTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestDemoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
